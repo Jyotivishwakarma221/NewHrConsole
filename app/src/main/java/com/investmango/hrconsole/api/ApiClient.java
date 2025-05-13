da8329e7cb5b1f55441af59cbe5f9ead8e2983f6
Khushi Saxena <khushisaxena@investmango.com>
2025-05-13 14:46:18 +0530

    Replace old project with new project

--- app/src/main/java/com/investmango/hrconsole/api/ApiClient.java
+++ app/src/main/java/com/investmango/hrconsole/api/ApiClient.java
@@ -38,10 +38,10 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 public class ApiClient extends Application {
 
     //development
-//          public static final String BASE_URL = "http://3.6.132.74:8080/";
+          public static final String BASE_URL = "http://3.6.132.74:8080/";
 
     // Live
-     public static final String BASE_URL = "https://api.virtualintelligence.co.in/";
+   //  public static final String BASE_URL = "https://api.virtualintelligence.co.in/";
 
 
     private final ApiInterface apiInterface;
